6757be6d78ae01c6d60084d4dab5bfa466dca430
nimmis <kjell.havneskold@gmail.com>
2017-01-08 23:31:50 +0100

    fix bootstrap for single machine

--- 0.6.1/docker-entrypoint.sh
+++ 0.6.1/docker-entrypoint.sh
@@ -9,7 +9,7 @@
 NETWORK_ADDR=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 
 # handle bootstap expected hosts
-BOOTSTRAP_EXPECT=${BOOTSTRAP_EXPECT:-1}
+# BOOTSTRAP_EXPECT=${BOOTSTRAP_EXPECT:-1}
 
 # set bind address
 BIND_ADDR=${BIND_ADDR:-$NETWORK_ADDR}
@@ -38,10 +38,46 @@ show_help() {
   echo "ERROR: $1"
   echo
   echo "start command with"
-  echo "server [<join address>] to run a consul server"
-  echo "agent  <join address>   to run a consul agent"
-  echo "reload                  to run consul with previous saved settings"
+  echo "server1                  to run a consul server, first/only server"
+  echo "server <join address>    to run more consul servers"
+  echo "agent  <join address>    to run a consul agent"
+  echo "reload                   to run consul with previous saved settings"
+  echo 
   exit 1
+}
+
+#
+# generate base config file
+#
+# init_config <data dir>
+#
+
+init_config() {
+
+CONFIG=$(cat <<EOF
+{
+  "node_name": "$HOSTNAME",
+  "datacenter": "$DATACENTER",
+  "data_dir": "$1",
+  "log_level": "$LOG_LEVEL",
+  "bind_addr": "$BIND_ADDR"
+EOF
+)
+
+}
+
+#
+# init_add <key> <data>
+#
+
+init_add(){
+
+CONFIG=${CONFIG}$(cat <<EOF
+,
+  "$1": $2
+EOF
+)
+
 }
 
 #
@@ -50,21 +86,12 @@ show_help() {
 
 write_agent_config() {
 
-cat > /data/agent/conf.d/00consul-agent.json  <<EOF
-{
-  "node_name": "$HOSTNAME",
-  "datacenter": "$DATACENTER",
-  "data_dir": "/data/agent",
-  "log_level": "$LOG_LEVEL",
-  "bind_addr": "$BIND_ADDR",
-  "dns_config": {
-                "allow_stale": true,
-                "max_stale": "1s"
-  		},
-  "start_join": ["$JOIN_IP"]
-}
-EOF
+  init_config "/data/agent"
+  init_add dns_config '{ "allow_stale": true, "max_stale": "1s" }'
+  init_add start_join "[\"$JOIN_IP\"]"
+  CONFIG=${CONFIG}$(printf "\n}\n" )
 
+  echo $CONFIG > /data/agent/conf.d/00consul-agent.json
 }
 
 #
@@ -73,27 +100,24 @@ EOF
 
 write_server_config() {
 
-cat > /data/server/conf.d/00consul-server.json <<EOF
-{
-  "server": true,
-  "node_name": "$HOSTNAME",
-  "datacenter": "$DATACENTER",
-  "data_dir": "/data/server",
-  "log_level": "$LOG_LEVEL",
-  "bind_addr": "$BIND_ADDR",
-  "client_addr": "0.0.0.0",
-  "dns_config": { "allow_stale": false },
-  "ui": true,
-  "bootstrap_expect": $BOOTSTRAP_EXPECT
-EOF
+  init_config "/data/server"
+  init_add server true
+  init_add client_addr '"0.0.0.0"'
+  init_add dns_config '{ "allow_stale": false }'
+  init_add ui true
 
-if [ -z $JOIN_IP ]; then
-  echo "}" >> /data/server/conf.d/00consul-server.json 
-else
-  printf ",\n  \"start_join\": [\"%s\"]\n}" $JOIN_IP >>  /data/server/conf.d/00consul-server.json
-fi
+  if [ -n "$BOOTSTRAP_EXPECT" ]; then
+    init_add bootstrap_expect $BOOTSTRAP_EXPECT
+  fi
 
+  if [ -n "$JOIN_IP" ]; then
+    init_add start_join "[\"$JOIN_IP\"]"
+  fi
+  CONFIG=${CONFIG}$(printf "\n}\n" )
+
+  echo $CONFIG > /data/server/conf.d/00consul-server.json
 }
+
 
 #
 # save information for reload
@@ -130,18 +154,21 @@ run_server() {
 
 do_run_agent() {
 
+      # create directory for agent
       mkdir -p /data/agent/conf.d/
 
       # agent requiers a join address
-
       if [ -z $JOIN_IP ]; then
         show_help "Join address missing"
       fi
 
+      # create the agent config file
       write_agent_config
 
+      # save state
       save_state agent
 
+      # run agent
       run_agent
 
 }
@@ -152,12 +179,21 @@ do_run_agent() {
 
 do_run_server() {
 
+      # create directory for server
       mkdir -p /data/server/conf.d/
 
+      # Either JOIN_IP or BOOTSTRAP_EXPECT must be defined
+      if [ -z $JOIN_IP ]  &&  [ -z $BOOTSTRAP_EXPECT ]; then                                              
+        show_help "Join address missing or BOOTSTRAP_EXPECT not defined"                                    
+      fi   
+
+      # create the server config file
       write_server_config
 
+      # save state
       save_state server 
-
+ 
+      # run server
       run_server
 
 }
@@ -200,6 +236,13 @@ if [ -n "$1" ]; then
   case $1 in
     agent)
       do_run_agent
+      ;;
+    
+    server1)
+      # setr expect to 1 if not set
+      BOOTSTRAP_EXPECT=${BOOTSTRAP_EXPECT:-1}
+
+      do_run_server
       ;;
 
     server) 
